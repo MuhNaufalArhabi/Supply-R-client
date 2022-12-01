1e957314bafa377b0d4e6ac171acc1e695b0bf10
Ｉｌｈａｍ <ilham.addina@gmail.com>
2022-12-02 02:13:30 +0700

    add product-details

--- supply-r/src/views/ProductDetail.jsx
+++ supply-r/src/views/ProductDetail.jsx
@@ -1,7 +1,51 @@
+import { Container, Col, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+
 export default function ProductDetail() {
-	return (
-		<>
-			<h1>Product Detail</h1>
-		</>
-	);
+  return (
+    <>
+      <Container>
+        <Row>
+          <div className="mt-5 mb-5">
+            <h1>Product Details</h1>
+          </div>
+        </Row>
+        <Row>
+          <Col sm={4}>
+            <img
+              src="https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/Untitled466619.png"
+              width={300}
+              height={400}
+            ></img>
+          </Col>
+          <Col sm={8}>
+            <div className="mt-5 mb-5">
+              <h5>Name </h5>
+              <h6>asas</h6>
+              <h5>Description </h5>
+              <h6>ada</h6>
+              <h5>Category </h5>
+              <h6>ada</h6>
+              <h5>Price </h5>
+              <h6>ada</h6>
+              <h5>Stock </h5>
+              <h6>ada</h6>
+              <Button variant="info">Add to Cart</Button>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Container>
+            <Row>
+              <h5>Images</h5>
+            </Row>
+          </Container>
+
+          <div className="mt-5 mb-5">
+            <h1>Seller Info</h1>
+          </div>
+        </Row>
+      </Container>
+    </>
+  );
 }
