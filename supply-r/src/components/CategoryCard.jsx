c48a1b4172d0c0c2c89abb0d66da75e5084df009
Ｉｌｈａｍ <ilham.addina@gmail.com>
2022-12-04 03:27:19 +0700

    add order page, carousel, chatroom, detail home

---
+++ supply-r/src/components/CategoryCard.jsx
@@ -0,0 +1,16 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+export default function CategoryCard() {
+  return (
+    <>
+      <div style={{ alignItems: "center" }}>
+        <img
+          src="https://padiumkm.id/_next/image?url=https%3A%2F%2Fsmb-padiumkm-images-public-prod.oss-ap-southeast-5.aliyuncs.com%2Fcategory%2Fdc2fb03086b65bb5560abfd23e2d62.png&w=1920&q=80"
+          style={{ width: "5rem" }}
+        />
+        <p>Category Title</p>
+      </div>
+    </>
+  );
+}
--- supply-r/src/components/ProductCard.jsx
+++ supply-r/src/components/ProductCard.jsx
@@ -1,7 +1,35 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { useNavigate } from "react-router-dom";
+
+
 export default function ProductCard() {
-	return (
-		<>
-			<h1>Product Card</h1>
-		</>
-	);
+  const navigate = useNavigate();
+  return (
+    <>
+      <Card style={{ width: "12rem" }} className="margin">
+        <Card.Img
+          variant="top"
+          src="https://smb-padiumkm-images-public-prod.oss-ap-southeast-5.aliyuncs.com/product/image/29102022/63575346cdb0741eddce73e3/635ca1f0729c230e0e897a98/64b021c89e7f7a5f3e53d65e9ad36c.JPG"
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>Rp. 200.000</Card.Text>
+          <Card.Text>Lokasi</Card.Text>
+          <Button
+            style={{
+              backgroundColor: "#204e64",
+              borderColor: "#204e64",
+              color: "white",
+            }}
+            onClick={() => {
+              navigate("/product-detail/1");
+            }}
+          >
+            Details
+          </Button>
+        </Card.Body>
+      </Card>
+    </>
+  );
 }
