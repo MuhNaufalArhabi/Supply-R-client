ac09bb07e3be67277b9ed439e3ba95d83577f207
Robby Kurniawan <thelazpiz@gmail.com>
2022-12-04 11:22:06 +0700

    navbar & footer

--- supply-r/src/views/HomePage.jsx
+++ supply-r/src/views/HomePage.jsx
@@ -1,15 +1,13 @@
 import ProductCard from "../components/ProductCard";
-import React, {useEffect} from "react";
-import {useSelector, useDispatch} from 'react-redux'
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { getProducts, productSelectors } from "../features/productSlice";
 export default function HomePage() {
-
-	const dispatch = useDispatch()
-	const products = useSelector(productSelectors.selectAll)
-	useEffect(()=> {
-		dispatch(getProducts())
-		
-	}, [dispatch])
+	const dispatch = useDispatch();
+	const products = useSelector(productSelectors.selectAll);
+	useEffect(() => {
+		dispatch(getProducts());
+	}, [dispatch]);
 	return (
 		<>
 			<h1>Home Page</h1>
