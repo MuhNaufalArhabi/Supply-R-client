f8f7db21c52d01606f66d60ee8a9d5cdb4d936df
Ｉｌｈａｍ <ilham.addina@gmail.com>
2022-12-05 11:57:11 +0700

    add static category card

--- supply-r/src/components/CategoryCard.jsx
+++ supply-r/src/components/CategoryCard.jsx
@@ -1,16 +1,102 @@
+import { Col, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
 export default function CategoryCard() {
   return (
     <>
-      <div style={{ alignItems: "center" }}>
-        <img
-          src="https://padiumkm.id/_next/image?url=https%3A%2F%2Fsmb-padiumkm-images-public-prod.oss-ap-southeast-5.aliyuncs.com%2Fcategory%2Fdc2fb03086b65bb5560abfd23e2d62.png&w=1920&q=80"
-          style={{ width: "5rem" }}
-        />
-        <p>Category Title</p>
-      </div>
+      <Row>
+        <Col>
+          <div style={{ alignItems: "center" }}>
+            <img
+              src="https://padiumkm.id/_next/image?url=https%3A%2F%2Fsmb-padiumkm-images-public-prod.oss-ap-southeast-5.aliyuncs.com%2Fcategory%2Fdc2fb03086b65bb5560abfd23e2d62.png&w=1920&q=80"
+              style={{ width: "5rem" }}
+            />
+            <p>Elektronik</p>
+          </div>
+        </Col>
+        <Col>
+          <div style={{ alignItems: "center" }}>
+            <img
+              src="https://padiumkm.id/_next/image?url=https%3A%2F%2Fsmb-padiumkm-images-public-prod.oss-ap-southeast-5.aliyuncs.com%2Fcategory%2F02e2e7f45059cc2e3a20f25a274033.png&w=1920&q=80"
+              style={{ width: "5rem" }}
+            />
+            <p>Pertukangan</p>
+          </div>
+        </Col>
+        <Col>
+          <div style={{ alignItems: "center" }}>
+            <img
+              src="https://padiumkm.id/_next/image?url=https%3A%2F%2Fsmb-padiumkm-images-public-prod.oss-ap-southeast-5.aliyuncs.com%2Fcategory%2F6a87e7f930a01909aec63b94b70123.png&w=1920&q=80"
+              style={{ width: "5rem" }}
+            />
+            <p>Jasa Konstruksi / Renovasi</p>
+          </div>
+        </Col>
+        <Col>
+          <div style={{ alignItems: "center" }}>
+            <img
+              src="https://padiumkm.id/_next/image?url=https%3A%2F%2Fsmb-padiumkm-images-public-prod.oss-ap-southeast-5.aliyuncs.com%2Fcategory%2F4bb98051a2622306f5909624ce1193.png&w=1920&q=80"
+              style={{ width: "5rem" }}
+            />
+            <p>Office / Stationary</p>
+          </div>
+        </Col>
+        <Col>
+          <div style={{ alignItems: "center" }}>
+            <img
+              src="https://padiumkm.id/_next/image?url=https%3A%2F%2Fsmb-padiumkm-images-public-prod.oss-ap-southeast-5.aliyuncs.com%2Fcategory%2Feeed10c248e740f515af9e88024325.png&w=1920&q=80"
+              style={{ width: "5rem" }}
+            />
+            <p>Jasa Perawatan / Peralatan dan Mesin</p>
+          </div>
+        </Col>
+        <Col>
+          <div style={{ alignItems: "center" }}>
+            <img
+              src="https://padiumkm.id/_next/image?url=https%3A%2F%2Fsmb-padiumkm-images-public-prod.oss-ap-southeast-5.aliyuncs.com%2Fcategory%2F1c77501b94f3a231bd09f092556fb5.png&w=1920&q=80"
+              style={{ width: "5rem" }}
+            />
+            <p>Souvenir / Merchandise</p>
+          </div>
+        </Col>
+        <Col>
+          <div style={{ alignItems: "center" }}>
+            <img
+              src="https://padiumkm.id/_next/image?url=https%3A%2F%2Fsmb-padiumkm-images-public-prod.oss-ap-southeast-5.aliyuncs.com%2Fcategory%2Fe230ad6855ac339b0856806971ca6a.png&w=1920&q=80"
+              style={{ width: "5rem" }}
+            />
+            <p>Jasa Event Organizer</p>
+          </div>
+        </Col>
+        <Col>
+          <div style={{ alignItems: "center" }}>
+            <img
+              src="https://padiumkm.id/_next/image?url=https%3A%2F%2Fsmb-padiumkm-images-public-prod.oss-ap-southeast-5.aliyuncs.com%2Fcategory%2Fcb6c1517d39f626b68b3c0bacff9fd.png&w=1920&q=80"
+              style={{ width: "5rem" }}
+            />
+            <p>Jasa Mandor dan Tenaga Kerja Lainnya</p>
+          </div>
+        </Col>
+        <Col>
+          <div style={{ alignItems: "center" }}>
+            <img
+              src="https://padiumkm.id/_next/image?url=https%3A%2F%2Fsmb-padiumkm-images-public-prod.oss-ap-southeast-5.aliyuncs.com%2Fcategory%2F373c668f1f45a0bcaf4ca7b38eab75.png&w=1920&q=80"
+              style={{ width: "5rem" }}
+            />
+            <p>Jasa Percetakan dan Media</p>
+          </div>
+        </Col>
+        <Col>
+          <div style={{ alignItems: "center" }}>
+            <img
+              src="https://padiumkm.id/_next/image?url=https%3A%2F%2Fsmb-padiumkm-images-public-prod.oss-ap-southeast-5.aliyuncs.com%2Fcategory%2Fdd2c9b5435b4ac91e9173b99361cd8.png&w=1920&q=80"
+              style={{ width: "5rem" }}
+            />
+            <p>Kesehatan</p>
+          </div>
+        </Col>
+      </Row>
     </>
   );
 }
--- supply-r/src/views/HomePage.jsx
+++ supply-r/src/views/HomePage.jsx
@@ -6,7 +6,6 @@ import PromoCarousel from "../components/PromoCarousel";
 import { Col, Container, Row } from "react-bootstrap";
 import CategoryCard from "../components/CategoryCard";
 export default function HomePage() {
-
   const dispatch = useDispatch();
   const products = useSelector(productSelectors.selectAll);
   useEffect(() => {
@@ -22,24 +21,21 @@ export default function HomePage() {
         </Row>
         <Row>
           <Col sm={12} className="mt-3 mb-3">
-          <Row>
-            <h4 style={{ color: "#204e64" }}>Category</h4>
-            <CategoryCard />
-            <CategoryCard />
-            <CategoryCard />
-          </Row>
+            <Row>
+              <h4 style={{ color: "#204e64" }}>Category</h4>
+              <CategoryCard />
+            </Row>
           </Col>
         </Row>
         <Row>
           <Col sm={12} className="mt-3 mb-3">
-          <Row>
-            <h4>Products</h4>
-            {products.map((product) => {
-              return <ProductCard product={product} key={product.id} />
-            })}
-          </Row>
+            <Row>
+              <h4>Products</h4>
+              {products.map((product) => {
+                return <ProductCard product={product} key={product.id} />;
+              })}
+            </Row>
           </Col>
-     
         </Row>
         <Row>
           <Col sm={12} className="mt-3 mb-3">
